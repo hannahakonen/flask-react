8a7d049afc43067bbd7ce5e1d1b1ff551fd01435
Johanna Hakonen <48389769+hannahakonen@users.noreply.github.com>
2024-01-09 10:53:16 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [currentTime, setCurrentTime] = useState(0);
+  useEffect(() => {
+    fetch('http://localhost:5000/time').then(res => res.json()).then(data => {
+      setCurrentTime(data.time);
+    });
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +24,7 @@ function App() {
         >
           Learn React
         </a>
+        <p>The current time is {currentTime}.</p>
       </header>
     </div>
   );
